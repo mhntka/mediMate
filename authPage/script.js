4faa45eb8b08eab393db9185454bdab8236aa828
mhntka <alik.wwe90@gmail.com>
2024-04-15 00:32:31 +0500

    v1.0

---
+++ authPage/script.js
@@ -0,0 +1,14 @@
+document.querySelector('.login-form').onsubmit = function(event) {
+    event.preventDefault();
+    // Здесь вы можете добавить проверку введенных данных и дальнейшую логику
+    var login = document.getElementById('login').value;
+    var password = document.getElementById('password').value;
+    console.log("Логин: " + login + " Пароль: " + password);
+    // Здесь должна быть логика авторизации
+  };
+  
+  document.getElementById('register').onclick = function() {
+    // Переход на страницу регистрации
+    window.location.href = 'register.html';
+  };
+  
